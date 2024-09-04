1981639488d22c35a6573cd2bd76aeeb18a56fec
Shashank Sigdel <shashanksigdel@Shashanks-Laptop.local>
2024-09-04 14:20:11 -0400

    Technologies

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -17,9 +17,15 @@ const Experience = () => {
               <div className='w-full max-w-xl lg:w-3/4'>
                 <h6 className='mb-2 font-bodyFont'>{experience.role} - <span className='text-sm text-designColor'>{experience.company}</span></h6>
                 <p className='mb-4 text-base font-normal text-gray-400'>{experience.description}</p>
-                {experience.technologies.map((tech, index)=>(
-                  <span key={index} className='mr-2 mt4 rounded bg-neutral-500 px-2 py-1 text-sm font-medium'>{tech}</span>
-                ))}
+                <div className='flex flex-wrap gap-2'>
+                  {experience.technologies.map((tech, index)=>(
+                    <span 
+                      key={index} 
+                      className='mr-2 mt-4 rounded bg-designColor px-2 py-1 text-sm font-medium text-white'>
+                      {tech}
+                    </span>
+                  ))}
+                </div>
               </div>
             </div>
         ))}
