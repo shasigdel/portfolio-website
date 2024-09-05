d6c51b9ae0007d3f577347e2a60bcf3ed0e296fc
Shashank Sigdel <shasigdel@gmail.com>
2024-09-04 20:09:36 -0400

    Added more teck stack

--- src/components/home/BannerIcons.js
+++ src/components/home/BannerIcons.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FaJava, FaJs, FaPython, FaReact } from 'react-icons/fa';
-import { SiGo, SiDjango, SiFlask, SiSpringboot } from 'react-icons/si';
+import { FaDocker, FaJava, FaJs, FaPython, FaReact } from 'react-icons/fa';
+import { SiGo, SiDjango, SiFlask, SiSpringboot, SiMongodb, SiPostgresql, SiKubernetes, SiGithub, SiTailwindcss } from 'react-icons/si';
 
 const BannerIcons = () => {
   return (
@@ -26,7 +26,7 @@ const BannerIcons = () => {
 
       {/* Frameworks Section */}
       <div className="text-center w-full">
-        <h1 className="text-2xl font-semibold mb-4">Frameworks</h1>
+        <h1 className="text-2xl font-semibold mb-4">Frameworks and CSS</h1>
         <div className="flex flex-wrap items-center justify-center gap-4 sm:gap-6">
           <span className="bannerIcon text-3xl sm:text-5xl">
             <FaReact />
@@ -39,6 +39,30 @@ const BannerIcons = () => {
           </span>
           <span className="bannerIcon text-3xl sm:text-5xl">
             <SiFlask />
+          </span>
+          <span className="bannerIcon text-3xl sm:text-5xl">
+            <SiTailwindcss />
+          </span>
+        </div>
+      </div>
+
+      <div className="text-center w-full">
+        <h1 className="text-2xl font-semibold mb-4">Databases and Tools</h1>
+        <div className="flex flex-wrap items-center justify-center gap-4 sm:gap-6">
+          <span className="bannerIcon text-3xl sm:text-5xl">
+            <SiMongodb />
+          </span>
+          <span className="bannerIcon text-3xl sm:text-5xl">
+            <SiPostgresql />
+          </span>
+          <span className="bannerIcon text-3xl sm:text-5xl">
+            <FaDocker />
+          </span>
+          <span className="bannerIcon text-3xl sm:text-5xl">
+            <SiKubernetes />
+          </span>
+          <span className="bannerIcon text-3xl sm:text-5xl">
+            <SiGithub />
           </span>
         </div>
       </div>
