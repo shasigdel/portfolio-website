a47828db913d60053a005e9e366255cbb4d93155
Shashank Sigdel <shasigdel@gmail.com>
2024-09-06 17:21:50 -0400

    resume-update

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,15 +1,13 @@
-import React from 'react'
-import { resume } from '../../assets/resume/ShashankSigdelResume-Updated.pdf'
+import React from 'react';
+import resume from '../../assets/resume/resume.pdf';
 
 const Resume = () => {
   return (
-     <section
-      id="resume">
-        <a href={resume} download='resume.pdf'>
-          
-        </a>
-      </section>
-  )
-}
+    <section id="resume">
+      <a href={resume}>
+      </a>
+    </section>
+  );
+}; 
 
-export default Resume
+export default Resume;
