a30682d03359fe4de7a3d0b808ac5aed6ba4ab29
Shashank Sigdel <shasigdel@gmail.com>
2024-11-07 12:42:35 -0500

    Experience Update

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -7,28 +7,36 @@ const Experience = () => {
     <section id="experience" className='border-b border-gray-600 pb-4 font-bodyFont'>
       
       <Title title="Experience" className="my-20 text-center text-4xl"/>
-        {EXPERIENCES.map((experience, index) => (
-          <div 
-            key={index}
-            className='mb-8 flex flex-wrap lg:justify-center'>
-              <div className='w-full lg:w-1/4'>
-                <p className='mb-2 text-sm text-neutral-400'>{experience.year}</p>
-              </div>
-              <div className='w-full max-w-xl lg:w-3/4'>
-                <h6 className='mb-2 font-bodyFont'>{experience.role} - <span className='text-sm text-designColor'>{experience.company}</span></h6>
-                <p className='mb-4 text-base font-normal text-gray-400'>{experience.description}</p>
-                <div className='flex flex-wrap gap-2'>
-                  {experience.technologies.map((tech, index)=>(
-                    <span 
-                      key={index} 
-                      className='mr-2 mt-4 rounded bg-designColor px-2 py-1 text-sm font-medium text-white'>
-                      {tech}
-                    </span>
-                  ))}
-                </div>
-              </div>
-            </div>
+{EXPERIENCES.map((experience, index) => (
+  <div key={index} className='mb-8 flex flex-wrap lg:justify-center text-lg'>
+    <div className='w-full lg:w-1/4'>
+      <p className='mb-2 text-sm text-neutral-400'>{experience.year}</p>
+    </div>
+    <div className='w-full max-w-xl lg:w-3/4'>
+      <h6 className='mb-2 font-bodyFont'>{experience.role} - <span className='text-sm text-designColor'>{experience.company}</span></h6>
+      <p className='mb-4 text-base font-normal text-gray-400'>
+      
+      <ul className='list-inside'>
+          {experience.description.map((desc, index) => (
+            <li key={index} className='relative pl-4'>
+              <span className='absolute left-0 text-designColor'>•</span>
+              {desc}
+            </li>
+          ))}
+        </ul>
+
+      </p>
+      <div className='flex flex-wrap gap-2'>
+        {experience.technologies.map((tech, index) => (
+          <span key={index} className='mr-2 mt-4 rounded bg-designColor px-2 py-1 text-sm font-medium text-white'>
+            {tech}
+          </span>
         ))}
+      </div> 
+    </div>
+  </div>
+))}
+           
     </section>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -36,28 +36,38 @@ export const EXPERIENCES = [
   year:'Jun 2024 - Present',
   role:'Software Engineer Intern',
   company: 'York College - IT Department',
-  description: 'Developed a scalable backend using Python and Flask, with a focus on enhancing LLM functionalities through AI-driven solutions using LangChain for YorkGPT. Created a responsive frontend using ReactJS to facilitate user interaction with the AI-powered YorkGPT. Assisted in the deployment and maintenance of AI-integrated software applications in production environments, ensuring smooth operation. Participated in code reviews to uphold high software quality standards, contributing to continuous improvement in the AI development process for YorkGPT.',
+  description: ['Developed a scalable backend using Python, enhancing LLM functionalities with LangChain.',
+  'Created a responsive frontend using ReactJS for user interaction with the SLM.',
+  'Assisted in deployment and maintenance of software applications in production.',
+  'Participated in code reviews to uphold software quality standards.'],
   technologies: ["Javascript","ReactJS", "LLM", "LangChain", "Postgres", "Flask", "RestAPI", "AI"]
 },
 {
   year:'Jun 2024 - Sept 2024',
   role:'Software Engineer Intern',
   company: 'Tech Incubator at Queens College',
-  description: 'Collaborated with cross-functional teams to design, develop, and test innovative software solutions. Focused on developing clean, maintainable, and efficient code using ReactJS, while utilizing MaterialUI to create intuitive user interfaces. Translated design mockups from Figma into functional code, adhering to design guidelines. Documented development processes and maintained technical documentation to ensure project sustainability and scalability.',
+  description: ['Collaborated with cross‑functional teams to design, code, and test software solutions.'
+  ,'Contributed to the development of clean, maintainable, and efficient code.'
+  ,'Documented development processes and maintained technical documentation.'],
   technologies: ["Javascript","ReactJS", "Figma"]
 }, 
 {
   year:'Feb 2024 - Apr 2024',
   role:'Software Engineer Intern',
   company: 'AuriStor Inc.',
-  description: 'Designed and developed a user-friendly dashboard for a file-sharing system using ReactJS and Tailwind CSS. Integrated complex APIs into the dashboard, collaborating closely with backend teams to ensure seamless data flow and functionality. Maintained application reliability and performance through debugging and troubleshooting. Leveraged Kubernetes and Docker for efficient container management and deployment, contributing to the scalability of the project.',
+  description: ['Designed and developed a dashboard for a file‑sharing system, leveraging ReactJS and TypeScript.'
+  ,'Integrated APIs into the dashboard, collaborating effectively with backend teams.'
+  ,'Utilized DSA to debug and troubleshoot application functionality, improving reliability and performance.'],
   technologies: ["Javascript","Tailwind CSS","ReactJS", "Docker", "Kubernetes", "Go"]
 }, 
 {
   year:'May 2022 - Feb 2023',
   role:'Software Engineer',
   company: 'BNY-Mellon',
-  description: 'Developed and maintained robust Java-based RESTful APIs to ensure secure data integration across various systems. Automated deployment pipelines using GitLab CI/CD, significantly enhancing the efficiency and reliability of application delivery. Integrated client data into BNY Mellon\'s data lake, improving data accessibility and consistency across the organization. Troubleshot and optimized Java applications, resulting in improved system stability and performance.',
+  description: ['Developed and maintained Java‑based RESTful APIs for secure data integration at BNY Mellon.'
+  ,'Automated deployment pipelines using GitLab CI/CD, leading to more efficient application delivery.'
+  ,'Integrated client data into BNY Mellon’s data lake, enhancing data accessibility and consistency.'
+  ,'Troubleshot and optimized Java applications, improving system stability and performance.'],
   technologies: ["Java","Rest API","Spring Boot", "DocuSign", "AppEngine"]
 }, 
 ]
