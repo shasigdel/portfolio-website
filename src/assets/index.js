1e751bc76b6646210188eeda40ec02b6dc1f1489
Shashank Sigdel <shasigdel@gmail.com>
2025-12-10 07:55:15 -0500

    Add new project images to index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,7 +11,9 @@ import shashank from '../assets/images/contact/shashank.jpg'
 import shashank2 from '../assets/images/contact/shashank2.jpg'
 import book from '../assets/images/projects/book.png'
 import contactImage from './images/contact/shashank.png'
-
+import hospital from '../assets/images/projects/hospital.png'
+import bank from '../assets/images/projects/bank.png'
+import woolly from '../assets/images/projects/woolly.png'
 
 export {
   logo,
@@ -27,4 +29,7 @@ export {
   shashank,
   shashank2,
   book,
+  hospital,
+  bank,
+  woolly
 }
