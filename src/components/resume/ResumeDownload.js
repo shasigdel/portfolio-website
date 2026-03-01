d37ce237d1e6123d2d12933a4201988387653cda
Shashank Sigdel <shasigdel@gmail.com>
2026-03-01 17:53:17 -0500

    Update ResumeDownload.js

--- src/components/resume/ResumeDownload.js
+++ src/components/resume/ResumeDownload.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { FaDownload } from 'react-icons/fa';
-import { resume } from '../../assets';
+import { resume_ss } from '../../assets';
 
 const ResumeDownload = () => {
     const [pdfUrl, setPdfUrl] = useState(null);
 
     useEffect(() => {
         // Pre-load the PDF
-        fetch(resume)
+        fetch(resume_ss)
             .then(response => response.blob())
             .then(blob => {
                 const url = window.URL.createObjectURL(blob);
@@ -24,7 +24,7 @@ const ResumeDownload = () => {
 
     const handleDownload = () => {
         const link = document.createElement('a');
-        link.href = pdfUrl || resume;
+        link.href = pdfUrl || resume_ss;
         link.setAttribute('download', 'ShashankSigdel_Resume.pdf');
         document.body.appendChild(link);
         link.click();
